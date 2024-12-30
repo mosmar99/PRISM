88e0b5c96f92940675aa257c3bdc9df98efca305
mosmar99 <47375043+mosmar99@users.noreply.github.com>
2024-12-30 10:51:41 +0100

    connect llm chat
    - todo: add buttons + prompt

--- src/general_chat.py
+++ src/general_chat.py
@@ -1,1 +1,19 @@
 import chainlit as cl
+import gemini
+
+async def chat_start():
+    welcome_message = (
+        "# **Welcome to PrismGPT!**\n\n"  
+        "PrismGPT assists nurses and doctors by providing insights into potential "
+        "side effects of drug combinations. Powered by cutting-edge AI, it's here "
+        "to support your decision-making process.\n\n"
+        "This chat is designed to help you with general medical questions."
+        "Feel free to ask anything you'd like to know about medications, treatments,"
+        "or general health advice.\n\n"
+        "Please note that the information provided here is not a substitute for professional medical advice."
+    )
+    await cl.Message(content=welcome_message).send()
+
+async def extraction(message: cl.Message):
+    response = await gemini.send_user_message(message.content)
+    await cl.Message(content=response).send()
